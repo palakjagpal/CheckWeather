50a561467f2991a725681cef0a0c61f2d96870c3
Palak Jagpal <palakjagpal70@gmail.com>
2026-03-14 21:02:28 +0530

    Updated Validation Logic

--- src/Weather_api.jsx
+++ src/Weather_api.jsx
@@ -7,37 +7,57 @@ const API_KEY = import.meta.env.VITE_API_KEY;
 function Weather_api(){
     const [weather,setweather] = useState(null);
     const [city, setcity] = useState("");
-    const [error, seterror] = useState(false);
+    const [error, seterror] = useState("");
+
+    const city_regex = /^[a-zA-Z]+(?:[\\s-][a-zA-Z]+)*$/;
 
     //async function
     async function getWeather(e){
         e.preventDefault();
-        try{
-            const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
-            setweather(res.data)
-            setcity("")
-            //setweather("")
-            seterror(false)
-        }catch(err){
-            seterror(true)
-            setweather(null)
-            console.log("Error : ",err)
+        if(city.trim() === ""){
+            seterror("Please enter a city name");
+            console.log("Please enter a city name");
+            return;
         }
+
+        if(!city_regex.test(city)){
+            console.log("Please enter a city");
+            seterror("Please enter a city");
+            return;
+        }
+
+            try{
+                const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+                setweather(res.data);
+                setcity("");
+                seterror("");
+            }catch(err){
+                seterror("Failed to fetch weather!");
+                setweather(null);
+                console.log("Error : ",err);
+            }
     }
 
     return(
         <>
+        {
+            error && (<div className="error"><p>{error}</p></div>)
+        }
+
             <div className="weatherForm">
                 <h2>Check Weather...</h2>
                 <br></br>
                 <form onSubmit={getWeather}>
-                    <input type="text" placeholder="Enter City" pattern="[a-zA-Z\s-]+" title="City name can only contain letters, spaces, and hyphens (e.g., New York or San-Francisco)" value={city} onChange={(e) => setcity(e.target.value)}></input>
+                    <input type="text" placeholder="Enter City"  
+                    value={city} 
+                    onChange={(e) => {setcity(e.target.value); seterror("");}}></input>
                     <br></br>
                     <button type="submit">SEARCH</button>
                 </form>
             </div>
 
             <div>
+
                 {weather && !error && 
                     (
                     <div className="weather"> 
@@ -59,9 +79,7 @@ function Weather_api(){
                     )
                 }
 
-                {
-                    error && (<div className="error"><p style="color:red;">Failed to fetch weather</p></div>)
-                }
+                
             </div>
             
         </>
